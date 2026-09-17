814093ae7d01bf63c9f3c60482be3a7fb8731b83
running-grass <hi@grass.show>
2026-09-17 11:18:43 +0800

    Revert "refactor(config): runtime logger 迁入 ctx.runtimeLogger 服务"
    This reverts commit 10d61b45746c90abd703890c17af37700b80c32e.

--- packages/habitat/core/config/index.ts
+++ packages/habitat/core/config/index.ts
@@ -15,11 +15,6 @@ export * from "./config-store.ts";
 export * from "./bootstrap-config.ts";
 export * from "./schemas/runtime-config.ts";
 export * from "./runtime-logger.ts";
-export {
-  RuntimeLoggerService,
-  fallbackRuntimeLogger,
-  mountRuntimeLoggerService,
-} from "./runtime-logger-service.ts";
 export * from "./context-window-injection.ts";
 export * from "./standalone-runtime-meta.ts";
 export * from "./build-meta.parse.ts";
